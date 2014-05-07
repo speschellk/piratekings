0f9fbad60b3c40ecfa08c74b6f5a2fd1816af147
amberr <rrockw@gmail.com>
2014-05-07 16:16:16 -0700

    shortened unnecessarily long hash

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -141,7 +141,7 @@ function initFullScreen() {
 
 function setHash() {
     var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    var string_length = 30;
+    var string_length = 5;
     var randomstring = '';
     for(var i = 0; i < string_length; i++) {
       var rnum = Math.floor(Math.random() * chars.length);
